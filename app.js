5c000708541745a259abbc70356076b87be7f007
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-16 15:41:32 -0500

    more setup

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require();
-const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const userRoutes = require('./controllers/userControllers');
+
+const app = express();
+
+// Body parser, reading data from body into req.body
+app.use(express.json());
+app.use(bodyParser.json());
 
 // serve the static files from React app
 app.use(express.static(path.join(__dirname, 'client', 'build')));
@@ -13,7 +20,7 @@ app.use('/api/v1/users', userRoutes);
 
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-  });
+});
   
 // error handling middleware
 app.use(globalErrorHandler);
