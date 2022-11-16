766684594751daca77f789169575acef9b850f96
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-16 09:47:47 -0500

    app file temp diabled

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const Pool = require('./db');
 
 // establish db connection
-Pool.then(() => console.log('PG DB connection successful!'))
+Pool.connect()
+.then(() => console.log('PG DB connection successful!'))
 .catch((err) => {
   console.log('PG DB connection failed!', err);
   Pool.end();
 });
 
-const app = require('./app');
+// const app = require('./app');
 
 // start server
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
-});
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}!`);
+// });
