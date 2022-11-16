691132e467e46209879b84a414084e7246d16945
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-16 12:27:07 -0500

    user routes and controllers init

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const express = require();
 const app = express();
 
 
+// routes
+app.use('/api/v1/users', userRoutes);
+
 
 module.exports = app;
---
+++ routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const {getUser} = require('../controllers/userControllers');
+
+router.route('/').get(getUser);
+
+module.exports = router;
