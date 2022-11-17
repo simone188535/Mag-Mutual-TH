7f2b71c9c8928edda8fbc336ae8b258124f5874e
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-16 20:07:07 -0500

    all controllers and routes defined

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,37 @@
 const catchAsync = require("../utils/catchAsync");
 // const AppError = require("../utils/appError");
 
-exports.getUser = catchAsync(async(req, res, next) => {
+
+exports.getAllUsers = catchAsync(async(req, res, next) => {
 
     // return next(new AppError('Error string', 400));
-    res.status(200);
+    res.status(200).json({
+        status: 'success',
+    });
+})
+exports.getUserById = catchAsync(async(req, res, next) => {
+
+    res.status(200).json({
+        status: 'success',
+    });
+});
+
+exports.getUserByEmail = catchAsync(async(req, res, next) => {
+
+    res.status(200).json({
+        status: 'success',
+    });
+});
+
+
+exports.getUsersInDateRange = catchAsync(async(req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+    });
+});
+
+exports.getUsersOfProfession = catchAsync(async(req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+    });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getUser } = require('../controllers/userControllers');
+const {
+  getAllUsers,
+  getUserById,
+  getUserByEmail,
+  getUsersInDateRange,
+  getUsersOfProfession,
+} = require("../controllers/userControllers");
 
-router.route('/').get(getUser);
+router.route("/").get(getAllUsers);
+router.route("/:id").get(getUserById);
+router.route("/email/:email").get(getUserByEmail);
+router.route("/date/:from-:to").get(getUsersInDateRange);
+router.route("/profession/:profession").get(getUsersOfProfession);
 
 module.exports = router;
