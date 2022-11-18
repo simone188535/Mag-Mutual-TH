1bf8b2112fbc1130ebddf0b700f3788cc71cbb73
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-17 20:32:11 -0500

    TableHelper init

---
+++ client/src/components/TableHelper/index.js
@@ -0,0 +1,16 @@
+
+function TableHelper() {
+
+  return (
+    <section>
+      <header className="flex-table-header">
+
+      </header>
+      <section className="flex-table-body">
+
+      </section>
+    </section>
+  );
+}
+
+export default TableHelper;
