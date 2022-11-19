47ed2ce483ce01af898b29022658f437c648859c
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-18 20:43:17 -0500

    total users moved

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -37,8 +37,8 @@ exports.getUser = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: "success",
-    allUsers: rows,
     totalUsers: rows.length,
+    allUsers: rows,
   });
 });
 
